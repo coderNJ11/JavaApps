27397e9b6fef9ab5de111d4e7af308fa272f23cc
RIshi K <rushikesh.khire@gmail.com>
2023-03-19 19:07:10 -0500

    Aws EKS deplooyment Via AWS ECR and Image in S3 , java maven project.

---
+++ eks/src/test/java/com/aws/ekspeloy/eks/EksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.ekspeloy.eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
