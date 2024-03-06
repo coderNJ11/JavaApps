16107ec865e4ad01654b390417a361b12637f6ab
RIshi K <rushikesh.khire@gmail.com>
2024-03-06 17:08:02 -0600

    update logrotate

--- proj1/src/main/resources/log_rotate.sh
+++ proj1/src/main/resources/log_rotate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Running logrotate..."
-logrotate /etc/logrotate.conf
+logrotate -s /app/logrotate.status /etc/logrotate.conf
 
 echo "Running node application..."
-echo $(node --max-http-header-size=131872 fromio) > /app/log/fromio.log 2>&1
+node --max-http-header-size=131872 fromio > /app/log/fromio.log 2>&1
 echo "Node application has finished running."
