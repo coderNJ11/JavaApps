bd770f6a57b9bb224e306903126ac7c54bc197da
RIshi K <rushikesh.khire@gmail.com>
2024-03-13 20:47:49 -0500

    KafkaProducer

--- proj1/src/main/java/Controller/NotificationProducerService.java
+++ proj1/src/main/java/Controller/NotificationProducerService.java
@@ -1,12 +1,14 @@
 package Controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import model.Notification;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class NotificationProducerService {
     private final KafkaTemplate<String, Notification> kafkaTemplate;
 
@@ -16,8 +18,21 @@ public class NotificationProducerService {
 
     // REST endpoint to save notifications to Kafka
     public Mono<ServerResponse> saveNotificationToKafka(ServerRequest request) {
-        return request.bodyToMono(Notification.class)
-                .doOnNext(notification -> kafkaTemplate.send("inBoundNotifq", notification.getRouteId(), notification))
-                .then(ServerResponse.ok().build());
+        return request.headers().header("X-Apple-Client-App-ID").stream().findFirst()
+                .map(clientAppId -> request.bodyToMono(Notification.class)
+                        .flatMap(notification -> Mono.fromFuture(() -> kafkaTemplate.send("inBoundNotifq", notification.getRouteId(), notification))
+                                .handle((recordMetadata, sink) -> {
+                                    if (recordMetadata != null) {
+                                        sink.next(recordMetadata);
+                                    } else {
+                                        sink.error(new RuntimeException("Error while sending notification to Kafka"));
+                                    }
+                                })
+                                .flatMap(result -> ServerResponse.ok().build())
+                                .doOnError(error -> log.error("Error while saving notification to Kafka {}", error))
+                                .onErrorMap(error -> new RuntimeException("Error while saving notification to Kafka"))
+                        )
+                )
+                .orElse(Mono.error(new IllegalArgumentException("X-Apple-Client-App-ID header not found")));
     }
 }
