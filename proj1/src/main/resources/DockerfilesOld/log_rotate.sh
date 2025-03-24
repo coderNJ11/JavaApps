f16ee7ddc40a9d68663c58aa453f336ca03ccb55
KTBFFH_personal <rushikesh.khire@gmail.com>
2025-03-23 21:49:46 -0500

    Update

--- proj1/src/main/resources/DockerfilesOld/log_rotate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Running logrotate..."
-logrotate -s /app/logrotate.status /etc/logrotate.conf
-
-echo "Running node application..."
-node --max-http-header-size=131872 fromio > /app/log/fromio.log 2>&1
-echo "Node application has finished running."
